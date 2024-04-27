77cf685ec33e06b4269148acdf68271278a28960
sneha-2510 <ranisnehaaa@gmail.com>
2024-04-27 20:31:16 +0530

    project completed

---
+++ src/error-page.jsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div id="error-page">
+      <h1>Opps!</h1>
+      <p>Sorry , an unexpected error has occured.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      <h1>WELCOME</h1>
+      The React Contact Book is a web application designed to help users manage their contacts efficiently. It provides a user-friendly interface for adding, editing, deleting, and searching contacts. The application utilizes React, a popular JavaScript library for building user interfaces, to create a dynamic and responsive experience.
+
+    </p>
+  );
+}
